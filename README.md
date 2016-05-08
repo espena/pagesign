8c491d9e36e804a2fd3c59d3cea666c331c93296
Espen Andersen <post@espenandersen.no>
2016-05-08 10:08:37 +0200

    working test version

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # pagesign
+
+HTML page compiler and GPG signer. GPG Tools and the gnupg PHP extension must be installed on the system,
+and a valid signing key must have been imported to the GPG keychain in order for this to work.
+
+Embeds images and url('xxx') resources into the HTML page as bas64 encoded data and signs the final file by
+appending comment blocks before and after the <html> tags.
+
+Usage:
+```php pagesign.php source destination fingerprint comments```
+
+_source_ : The source HTML file name.
+
+_destination_ : The destination HTML file name.
+
+_fingerprint_ : The fingerprint for the signing key. Must exist in GPG keychain.
+
+_comments_ : Text template of the information comment block to be inserted immediately before the <html> start tag.
+             The tag {keyinfo} can be used anywhere in this text file to output basic information of the signing key.
--- src/pagesign.php
+++ src/pagesign.php
@@ -1,1 +1,25 @@
-<?php ?>
+<?php
+  function main( $argc, $argv ) {
+    if( $argv > 2 && file_exists( $argv[ 1 ] ) ) {
+      $html = file_get_contents( $argv[ 1 ] );
+      preg_match_all( '/(src=|href=|url\()(\s*)[\"\']([^\"\']{1,255})[\"\'](\s*\))?/i', $html, $m, PREG_SET_ORDER );
+      $base64 = array();
+      foreach( $m as $attr ) {
+        if( !file_exists( $attr[ 3 ] ) ) continue;
+        $lnEnding = ( $attr[ 1 ] == 'url(' ? "\\\n" : "\n" );
+        $mime = mime_content_type( $attr[ 3 ] );
+        $enc = "data:$mime;base64," . base64_encode( file_get_contents( $attr[ 3 ] ) );
+        $html = str_replace( $attr[ 0 ], str_replace( $attr[ 3 ], trim( chunk_split( $enc, 8192, $lnEnding ), "\\\n" ), $attr[ 0 ] ), $html );
+      }
+      $fpr = $argv[ 3 ];
+      $keyinfo = str_replace( "\n", "\n  ", trim( `gpg --list-keys --with-fingerprint $fpr` ) ) . "\n";
+      $header = file_exists( $argv[ 4 ] ) ? str_replace( '{keyinfo}', $keyinfo, file_get_contents( $argv[ 4 ] ) ) : $keyinfo;
+      $gpg = gnupg_init();
+      gnupg_addsignkey( $gpg, $fpr );
+      $html = preg_split( '/<[\/ ]?html/', $html );
+      $html = $html[ 0 ] . "<!--\n" . gnupg_sign( $gpg, $header . " -->\n<html" . $html[ 1 ] . "</html>\n<!--\n" ) . "-->";
+      file_put_contents( $argv[ 2 ], $html );
+    }
+  }
+  main( $argc, $argv );  
+?>
